e338f410ea1a0b6e319e94526dff88dbe0dc9131
Daniel Martinez <dmartmar@mercadona.es>
2022-07-20 17:16:51 +0200

    feat: Recogemos valor de formulario switches y lo anyadimos a una persona

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.formBuilder.group({
+    genero: [ , Validators.required ],
+    notificaciones: [ , Validators.required ],
+    condiciones: [ , Validators.requiredTrue ]
+  });
+
+  persona = {
+    genero: 'F',
+    notificaciones: true
+  }
+
+  constructor( private formBuilder: FormBuilder ) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({ //El reset permite asignar un objeto que tiene menos propiedades que el formGroup
+      ...this.persona,
+      condiciones: false
+    });
+  }
+
+  guardar() {
+    const formValue = {...this.miFormulario.value};
+    delete formValue.condiciones;
+
+    this.persona = formValue;
   }
 
 }
