bdca71b8344947f50b7e268dbebf418e7267d88d
Daniel Martinez <dmartmar@mercadona.es>
2022-07-20 16:42:34 +0200

    feat: Creacion y eliminacion de arrayControls de forma dinamica

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -7,7 +7,7 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
   miFormulario: FormGroup = this.formBuilder.group({
     nombre: [ , [Validators.required, Validators.minLength(3)] ],
@@ -17,18 +17,30 @@ export class DinamicosComponent implements OnInit {
     ], Validators.required )
   });
 
+  nuevoFavorito: FormControl = this.formBuilder.control('', Validators.required);
+
   get favoritosArr(): FormArray {
     return this.miFormulario.get('favoritos') as FormArray;
   }
 
   constructor( private formBuilder: FormBuilder ) { }
 
-  ngOnInit(): void {
-  }
-
   isInvalidField( field: string ) {
     return this.miFormulario.controls[field].errors
           && this.miFormulario.controls[field].touched;
+  }
+
+  agregarFavorito() {
+
+    if(this.nuevoFavorito.invalid)
+      return;
+
+    this.favoritosArr.push( this.formBuilder.control( this.nuevoFavorito.value, Validators.required ) );
+    this.nuevoFavorito.reset();
+  }
+
+  eliminarFavoritos( index: number ) {
+    this.favoritosArr.removeAt(index);
   }
 
   guardar() {
