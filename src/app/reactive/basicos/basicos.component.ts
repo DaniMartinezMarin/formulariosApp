7a2a5adff110f3fd390074badca2d0b730cd4947
Daniel Martinez <dmartmar@mercadona.es>
2022-07-20 15:35:24 +0200

    feat: anyadidas validaciones y reseteo de validadores en basicos

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -7,7 +8,7 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
   styles: [
   ]
 })
-export class BasicosComponent {
+export class BasicosComponent implements OnInit {
 
   /* miFormulario: FormGroup = new FormGroup({
     nombre: new FormControl('RTX 4080ti'),
@@ -15,12 +16,35 @@ export class BasicosComponent {
     existencias: new FormControl(0)
   }); */
 
+  miFormulario: FormGroup = this.formBuilder.group({
+    nombre: [ , [Validators.required, Validators.minLength(3)] ],
+    precio: [ , [Validators.required, Validators.min(0)] ],
+    existencias: [ , [Validators.required, Validators.min(0)] ],
+  });
+
   constructor( private formBuilder: FormBuilder ) { }
 
-  miFormulario: FormGroup = this.formBuilder.group({
-    nombre: [ 'RTX 4080ti' ],
-    precio: [ 2000 ],
-    existencias: [ 0 ],
-  });
+  ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'RTX 4080ti',
+      precio: 2000
+    })
+  }
+
+  isInvalidField( field: string) {
+    return this.miFormulario.controls[field].invalid
+          && this.miFormulario.controls[field].touched;
+  }
+
+  guardar(): void {
+
+    if(this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
+  }
 
 }
