238fb45bb370a4341b44b46a74ae3710ec715bd8
Daniel Martinez <dmartmar@mercadona.es>
2022-07-20 16:06:44 +0200

    feat: FormArray creado y asociado a template

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -8,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.formBuilder.group({
+    nombre: [ , [Validators.required, Validators.minLength(3)] ],
+    favoritos: this.formBuilder.array( [
+      ['Pokemon', Validators.required],
+      ['Zelda', Validators.required]
+    ], Validators.required )
+  });
+
+  get favoritosArr(): FormArray {
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
+
+  constructor( private formBuilder: FormBuilder ) { }
 
   ngOnInit(): void {
+  }
+
+  isInvalidField( field: string ) {
+    return this.miFormulario.controls[field].errors
+          && this.miFormulario.controls[field].touched;
+  }
+
+  guardar() {
+
+    if(this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
   }
 
 }
