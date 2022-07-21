d8a4ef46ab5ef7fa9631117c93544b5a4f043133
Daniel Martinez <dmartmar@mercadona.es>
2022-07-21 13:35:48 +0200

    feat: Create register template

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { RegistroComponent } from './pages/registro/registro.component';
+
 import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
 
 @NgModule({
   declarations: [
-    RegistroComponent,
-    LoginComponent
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     CommonModule,
