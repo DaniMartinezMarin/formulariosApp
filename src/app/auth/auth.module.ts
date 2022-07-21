a165190113ea0bdaf9a7d73d00a737d366ee5887
Daniel Martinez <dmartmar@mercadona.es>
2022-07-21 14:04:49 +0200

    feat: Validaciones personalizadas de email y username

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
@@ -14,7 +15,8 @@ import { RegistroComponent } from './pages/registro/registro.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+  noPuedeSerAgathor( control: FormControl ) {
+    const valor: string = control.value?.trim().toLowerCase();
+
+    if(valor === 'agathor') {
+      return {
+        noAgathor: true
+      }
+    }
+
+    return null;
+  }
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.pattern(this.nombreApellidoPattern)] ],
+    email: ['', [Validators.required, Validators.pattern(this.emailPattern)] ],
+    username: ['', [Validators.required, this.noPuedeSerAgathor] ], //No se ponen los parentesis de ejecutar el metodo
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'Dani Martinez',
+      email: 'test@gmail.com'
+    })
+  }
+
+  isInvalidField( field: string ) {
+    return this.miFormulario.get(field)?.invalid && this.miFormulario.get(field)?.touched;
+  }
+
+  submitFormulario() {
+
+    if(this.miFormulario.invalid)
+    {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    this.miFormulario.reset();
   }
 
 }
